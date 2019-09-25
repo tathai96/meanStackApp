e4775df25395e8c10a20d60aebf391e553a3e27a
Tathagata <tathai96@gmail.com>
2019-09-25 23:04:02 +0530

    material design and toolbar

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
   templateUrl: './post-create.component.html',
   styleUrls: ['./post-create.component.css']
 })
-export class PostCreateComponent implements OnInit {
+export class PostCreateComponent {
+  enteredValue = '';
+  newPost = '';
 
-  constructor() { }
-
-  ngOnInit() {
+  onAddPost() {
+    console.dir();
+    this.newPost = this.enteredValue;
   }
-
 }
