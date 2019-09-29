e9a9bdd35145fac728af12138ac63c7f5df46e50
Tathagata <tathai96@gmail.com>
2019-09-29 22:42:26 +0530

    fetching post /api/posts

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('First Middleware');
-    // next();
-});
-
-app.use((req, res, next) => {
-    res.send("Hello from express");
+app.use('/api/posts', (req, res, next) => {
+  const posts = [{
+      id: "asdjd316542",
+      title: "First server side post",
+      content: "This is coming from server"
+    },
+    {
+      id: "figjnl84521165",
+      title: "Second server side post",
+      content: "This is coming from server"
+    }
+  ]
+  res.status(200).json({
+    message: 'Posts fetched successfully!',
+    posts: posts
+  });
 });
 
 module.exports = app;
